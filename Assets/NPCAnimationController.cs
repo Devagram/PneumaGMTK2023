8f9c838d55c96c3bb4f5fbebf6f6af3da559e265
Thomas Anderson <tomp.45a@gmail.com>
2023-07-09 11:24:06 -0500

    Stuff

--- Assets/NPCAnimationController.cs
+++ Assets/NPCAnimationController.cs
@@ -6,6 +6,18 @@ public class NPCAnimationController : MonoBehaviour
 {
     [SerializeField]
     public Animator animator;
+    [SerializeField]
+    public AudioSource audioSource;
+    [SerializeField]
+    public AudioClip idleClip;
+    [SerializeField]
+    public AudioClip runClip;
+    [SerializeField]
+    public AudioClip attackClip;
+    [SerializeField]
+    public AudioClip hurtClip;
+    [SerializeField]
+    public AudioClip dieClip;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,30 +26,37 @@ public class NPCAnimationController : MonoBehaviour
 
     public void PlayIdleAnimation()
     {
+        audioSource.PlayOneShot(idleClip);
         Debug.Log(name + "IdleTrigger");
         animator.SetTrigger("IdleTrigger");
     }
 
     public void PlayRunAnimation()
     {
+        audioSource.clip = runClip;
+        audioSource.loop = true;
+        audioSource.Play();
         Debug.Log(name + "RunTrigger");
         animator.SetTrigger("RunTrigger");
     }
 
     public void PlayAttackAnimation()
     {
+        audioSource.PlayOneShot(attackClip);
         Debug.Log(name + "AttackTrigger");
         animator.SetTrigger("AttackTrigger");
     }
 
     public void PlayHurtAnimation()
     {
+        audioSource.PlayOneShot(hurtClip);
         Debug.Log(name + "HurtTrigger");
         animator.SetTrigger("HurtTrigger");
     }
 
     public void PlayDeathAnimation()
     {
+        audioSource.PlayOneShot(dieClip);
         Debug.Log(name + "DeathTrigger");
         animator.SetTrigger("DeathTrigger");
     }
