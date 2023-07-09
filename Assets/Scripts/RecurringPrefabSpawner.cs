9b7cc96796a744055f1b7701bf266f6c9fdc85dd
Geode890 <63738626+Geode890@users.noreply.github.com>
2023-07-08 23:06:23 -0400

    Added spider stuff

--- Assets/Scripts/RecurringPrefabSpawner.cs
+++ Assets/Scripts/RecurringPrefabSpawner.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class RecurringPrefabSpawner : MonoBehaviour
 {
     [SerializeField]
     private GameObject prefab;
     [SerializeField]
     private float interval;
+    private Animator animator;
 
     void Start()
     {
+        animator = GetComponent<Animator>();
+
         if (prefab != null)
         {
             InvokeRepeating("SpawnPrefab", interval, interval);
@@ -17,6 +21,7 @@ public class RecurringPrefabSpawner : MonoBehaviour
 
     private void SpawnPrefab()
     {
-        Instantiate(prefab, transform.position, transform.rotation);
+        animator.SetTrigger("SpawnPrefab");
+        Instantiate(prefab, transform.position, transform.rotation);;
     }
 }
