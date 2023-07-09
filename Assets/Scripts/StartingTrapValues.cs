b52942dfc148345e5705a68e9db65bc21d2a46b5
Geode890 <63738626+Geode890@users.noreply.github.com>
2023-07-09 10:52:17 -0400

    Added default trap inventory values

---
+++ Assets/Scripts/StartingTrapValues.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class StartingTrapValues : MonoBehaviour
+{
+    public int mouthTrapAmount = 2;
+
+    public int upArrowTrapAmount = 1;
+    public int rightArrowTrapAmount = 1;
+    public int downArrowTrapAmount = 1;
+    public int leftArrowTrapAmount = 1;
+
+    public int spiderAmount = 2;
+
+    public int springTrapAmount = 2;
+
+    public int chestAmount = 4;
+}
